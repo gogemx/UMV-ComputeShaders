33c11baeb940a781f06c24aca32141a2bd20f881
gogemx <go@ybna.net>
2022-11-01 11:06:13 +0100

    pseudo random noise in compute shaders

---
+++ Assets/3.Randomize/Randomize.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Randomize : MonoBehaviour
+{
+    [SerializeField] private ComputeShader computeShader;
+    [SerializeField] private Vector2Int resolution = new Vector2Int(512, 256);
+
+    private RenderTexture renderTexture = null;
+    private int kernel = 0;
+
+    private void Start()
+    {
+        if (renderTexture == null)
+        {
+            renderTexture = new RenderTexture(resolution.x, resolution.y, 16, RenderTextureFormat.ARGB32);
+            renderTexture.enableRandomWrite = true;
+            renderTexture.Create();
+        }
+
+        int kernel = computeShader.FindKernel("Randomize");
+
+        computeShader.SetVector("resolution", new Vector4(resolution.x, resolution.y));
+        computeShader.SetTexture(kernel, "target", renderTexture);
+    }
+
+    private void Update()
+    {
+        computeShader.SetFloat("time", 0.0001f * Time.realtimeSinceStartup);
+        computeShader.Dispatch(kernel, renderTexture.width / 8, renderTexture.height / 8, 1);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(renderTexture, destination);
+    }
+}
